0198992ce1ae6bd41947b792350a944085ecf1c7
Emilie Ickart <emi.ickart@gmail.com>
2022-06-02 11:55:51 +0200

    Ft-Add path to display index.html

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const host = 'localhost';
 const port = 8080;
+const path = require('path');
+
+// Redirection vers la page index.html
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Server setup
 app.listen(port, host, () => {
