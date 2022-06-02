80dcfab9ee5b3e4fbb0ffbd24c9f0a9df2c595d8
Emilie Ickart <emi.ickart@gmail.com>
2022-06-02 23:40:14 +0200

    Ft-Update Parkings Schema

--- schemas/parkings.js
+++ schemas/parkings.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 const { schema } = mongoose;
 const parkingSchema = new Schema({
+    id: { type: Number, required: true },
     name: { type: String, required: true },
     type: { type: String, required: true },
     city: { type: String, required: true },
