d15b5cbbb99e89f3f12b4a2c99f543ab7a2e0b58
Emilie Ickart <emi.ickart@gmail.com>
2022-06-04 11:34:51 +0200

    Change MongoUri name

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const reservationsRoutes = require('./routes/reservations');
 async function connectToDB() {
     // Use connect method to connect to the server
     await mongoose
-        .connect(process.env.MONGO_URL, {
+        .connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         })
